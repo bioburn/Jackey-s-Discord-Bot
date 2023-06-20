46e834aaf81738765b06a39ec74f45d01847ff42
Jackey <Jackey@DESKTOP-GC5E9H1>
2023-06-20 07:28:10 -0700

    Add project files.

---
+++ Discord Bot/Program.cs
@@ -0,0 +1,68 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord_Bot;
+using System.Reflection;
+
+
+public class Program
+{
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    private DiscordSocketClient? _client;
+    private CommandService? _commands;
+
+    private CommandHandler? commandHandler;
+
+    public async Task MainAsync()
+    {
+        var config = new DiscordSocketConfig
+        {
+            // Set the desired GatewayIntents
+            GatewayIntents = GatewayIntents.Guilds | GatewayIntents.GuildMessages | GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+        };
+
+        _client = new DiscordSocketClient(config);
+        
+
+        _client.Log += Log;
+
+        _commands = new CommandService();
+        await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+
+        commandHandler = new CommandHandler(_client, _commands);
+        await commandHandler.InstallCommandsAsync();
+
+        //  You can assign your bot token to a string, and pass that in to connect.
+        //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+        //var token = "";
+
+        // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+        // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+        var token = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)+"/token.txt");
+        // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+        //_client.SlashCommandExecuted += SlashCommandHandler;
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+       
+
+        
+
+        
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+
+    private async Task SlashCommandHandler(SocketSlashCommand command)
+    {
+        await command.RespondAsync($"You executed {command.Data.Name}");
+    }
+}
+    
---
+++ Discord Bot/VoiceChatModule.cs
@@ -0,0 +1,83 @@
+﻿using Discord;
+using Discord.Audio;
+using Discord.Commands;
+using System.Diagnostics;
+
+
+namespace Discord_Bot
+{
+    public class VoiceChatModule : ModuleBase<SocketCommandContext>
+    {
+        // ~play stellar stellar.wav
+        [Command("play", RunMode = RunMode.Async)]
+        [Summary("Plays a youtube video.")]
+
+        public async Task JoinChannel(string url,IVoiceChannel channel = null)
+        {
+            // Get the voice channel the user is in
+            var voiceChannel = (Context.Message.Author as IGuildUser)?.VoiceChannel;
+
+           
+            if (voiceChannel == null) { await Context.Channel.SendMessageAsync("User must be in a voice channel, or a voice channel must be passed as an argument."); return; }
+
+            // For the next step with transmitting audio, you would want to pass this Audio Client in to a service.
+            var audioClient = await voiceChannel.ConnectAsync();
+
+
+            
+            //await SendAsync(audioClient, "test.mp3");
+            //await SendAsyncURL(audioClient, "https://www.youtube.com/watch?v=k0rj-Y0nMcc");
+            await SendAsyncURL(audioClient, url);
+        }
+
+        private Process CreateStream(string path)
+        {
+            return Process.Start(new ProcessStartInfo
+            {
+                FileName = "ffmpeg",
+                Arguments = $"-hide_banner -loglevel panic -i \"{path}\" -ac 2 -f s16le -ar 48000 pipe:1",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+            });
+        }
+
+        private async Task SendAsync(IAudioClient client, string path)
+        {
+            // Create FFmpeg
+            using (var ffmpeg = CreateStream(path))
+            using (var output = ffmpeg.StandardOutput.BaseStream)
+            using (var discord = client.CreatePCMStream(AudioApplication.Mixed))
+            {
+                try { await output.CopyToAsync(discord); }
+                catch(Exception ex) { Console.WriteLine($"{ex.Message}"); }
+                finally { await discord.FlushAsync(); }
+            }
+        }
+
+        private async Task SendAsyncURL(IAudioClient client, string url)
+        {
+            // Create FFmpeg
+            using (var output = ConvertURLToPcm(url))
+            using (var discord = client.CreatePCMStream(AudioApplication.Mixed))
+            {
+                try { await output.CopyToAsync(discord); }
+                catch (Exception ex) { Console.WriteLine($"{ex.Message}"); }
+                finally { await discord.FlushAsync(); }
+            }
+        }
+
+        private Stream ConvertURLToPcm(string url)
+        {
+            string args = $"/C yt-dlp --verbose --ignore-errors -o - {url} | ffmpeg -err_detect ignore_err -i pipe:0 -ac 2 -f s16le -ar 48000 pipe:1";
+            var ffmpeg = Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = args,
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            });
+            return ffmpeg.StandardOutput.BaseStream;
+        }
+
+    }
+}
